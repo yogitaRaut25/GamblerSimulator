8d16769a736ad3587c23298134d13c83a73dbe7c
yogita <yogitasraut25@gmail.com>
2022-02-09 12:36:03 +0530

    Added: Welcome message to game

---
+++ src/WelcomeToGame.java
@@ -0,0 +1,9 @@
+
+public class WelcomeToGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/bridgelab/workshop/WelcomeToGame.java
@@ -0,0 +1,8 @@
+package com.bridgelab.workshop;
+
+public class WelcomeToGame {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to gambler simulator game");
+	}
+}
