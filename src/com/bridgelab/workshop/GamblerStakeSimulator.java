ce4f9736519391a3500a9a3302e279d0d38c1761
yogita <yogitasraut25@gmail.com>
2022-02-09 12:39:12 +0530

    Added: Gambler stake and bet logic

---
+++ src/com/bridgelab/workshop/GamblerStakeSimulator.java
@@ -0,0 +1,11 @@
+package com.bridgelab.workshop;
+
+public class GamblerStakeSimulator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to gambler simulator game");
+		final int stake = 100; //stake for rvery day
+		final int bet = 1; //bet for every game
+	}
+
+}
